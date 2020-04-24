0e2a94860c5d59a7a93caa7181e606b7dda5a799
samuelaidoo45 <samuelaidoo45@gmail.com>
2020-04-24 02:08:15 -0400

    =

--- routes/index.js
+++ routes/index.js
@@ -38,6 +38,15 @@ const isLoggedin= (req,res,next)=>{
 }
 
 //
+const isLoggedin_4_logout= (req,res,next)=> {
+    if(req.isAuthenticated()){
+        next();    
+    }else{
+//        req.flash('error_message',`You need to logout first.`);
+        res.redirect('/');
+    }
+}
+
 
 
 //sign up endpoint
@@ -131,3 +140,259 @@ router.get('/cart',(req,res)=> {
     res.render('routes_UI/cart',{cart, itemsArray, user:req.user});
 })
 
+
+//Checkout endpoint get
+router.get('/checkout', isLoggedin,(req,res)=>{
+    let error_message = req.flash('error_message')[0];
+    if(!req.session.cart){
+        req.flash('error_message',`Add some itmes first.`);
+        res.redirect('/cart');
+    }else{
+        res.render('routes_UI/checkout', {user:req.user,totalPrice:req.session.totalPrice,error_message:error_message});
+    }
+})
+
+//Checkout endpoint post
+router.post('/checkout',(req,res)=> {
+    
+    var stripe = require("stripe")("sk_test_JFyJNPEu7Ld6DOjnMxZU5CTY");
+
+    stripe.charges.create({
+      amount: req.session.cart.totalPrice * 100,
+      currency: "usd",
+      source: req.body.stripeToken, // obtained with Stripe.js
+      description: "Charge for products."
+    }, function(err, charge) {
+      if(err){
+          console.log(err);
+          req.flash('error_message',err.message);
+          return res.redirect('/checkout');
+      }
+        if(charge){
+            
+            console.log(charge);
+            const newOrder= new Order({
+                userEmail:req.user.email,
+                order:req.user.cart,
+                name:req.body.name,
+                address:req.body.address,
+                paymentId:charge.id,     
+            })
+            newOrder.save();
+            
+        
+           User.findOne({email:req.user.email}).then((user)=> {
+               
+//              user.orders.push(user.cart);
+//              console.log(user.orders[0]);
+              req.session.cart= null;
+              user.cart= null;
+               
+              user.save().then(()=> {
+                  req.flash('success_message',`successfully bought product(s)!`);
+                  res.redirect('/');
+              })
+           })
+        }
+    });
+    
+})
+
+//Index endpoint
+router.get('/', (req,res)=>{
+    let success_message= req.flash('success_message');
+
+    Product.find().then((products)=>{
+        let productChunks= [];
+        const chunkSize= r;
+        for(let i=0; i<products.length; i +=chunkSize){
+            productChunks.push(products.slice(i,i+chunkSize));
+        }
+
+        if(req.isAuthenticated()){
+            User.findOne({email:req.user.email}).then((user)=>{
+                let x= JSON.stringify(req.session.cart);
+                let y= JSON.stringify(user.cart);
+                let z= (x !== y);
+
+                console.log(user.cart);
+                console.log(req.session.cart);
+                if(req.session.cart && z){
+                    
+                    let cart= new Cart(user.cart ? user.cart : {} );
+                
+                    cart.add2(req.session.cart);
+                    cart.generateArray();
+                    req.session.cart= cart;
+                    user.cart= cart;
+                    
+                    
+                }else{
+                    req.session.cart=user.cart;
+                } 
+                
+                console.log(req.session.cart);
+                user.save().then(()=>{
+                    res.render('routes_UI/index', {productChunks, user:req.user,success_message});
+                })
+
+            })
+        }else{
+            res.render('routes_UI/index', {productChunks});
+        }
+    })
+});
+
+router.get('/logout',isLoggedin_4_logout,(req, res)=>{
+ 
+    req.session.destroy();
+    req.logout();
+    res.redirect('/login');
+    
+});
+
+
+router.post('/signup',(req,res)=> {
+       
+    if(req.body.password!==req.body.confirmPassword){
+        req.flash('error_message',`Passwords do not match`);
+        res.redirect('/signup');
+    }else{
+        
+        User.findOne({ email: req.body.email}).then((user)=> {
+            if(user){
+               req.flash('error_message',`A user with this email already exists`);
+               res.redirect('/signup');
+            }else{
+                    bcrypt.genSalt(10, function(err, salt) {
+                    bcrypt.hash(req.body.password, salt, function(err, hash) {
+
+                        const user= new User({
+                                username:req.body.username,
+                                email:req.body.email,
+                                password:hash
+                            });
+
+                        user.save().then(()=> {
+                            req.flash('success_message',`You have registered successfully, please login`);
+                            res.redirect('/login');
+                        });
+                     });
+                  });
+            }
+        })   
+    }   
+})
+
+passport.use(new LocalStrategy({usernameField: 'email'},
+  (email, password, done)=> {
+    
+    User.findOne({email:email}).then((user)=> {
+        
+      if (!user) {
+        return done(null, false);
+      }
+        
+        bcrypt.compare(password, user.password,(err, matched)=> {
+            
+                if(matched){
+                    return done(null, user);
+                }
+                else{
+                    return done(null, false);
+                }
+        });
+    })
+   }
+));
+
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+
+router.post('/login',
+  passport.authenticate('local'
+                        , {successRedirect: '/',
+                          failureRedirect: '/login',
+                          failureFlash: 'Invalid email or password.',
+                          successFlash: 'You are logged in, now you can buy products.'}
+                       ));
+
+ const products= [
+  
+                    new Product({
+                            imagePath: 'https://upload.wikimedia.org/wikipedia/en/7/7b/Personal_Computer.JPG',
+                            title: 'Simple laptop',
+                            description: 'Very nice simple laptop',
+                            price: 200
+                        }),
+                        new Product({
+                            imagePath: 'https://upload.wikimedia.org/wikipedia/en/7/7b/Personal_Computer.JPG',
+                            title: 'Simple laptop',
+                            description: 'Very nice simple laptop',
+                            price: 200
+                        }),
+                        new Product({
+                            imagePath: 'https://upload.wikimedia.org/wikipedia/en/7/7b/Personal_Computer.JPG',
+                            title: 'Simple laptop',
+                            description: 'Very nice simple laptop',
+                            price: 200
+                        }),
+                        new Product({
+                            imagePath: 'https://upload.wikimedia.org/wikipedia/en/7/7b/Personal_Computer.JPG',
+                            title: 'Simple laptop',
+                            description: 'Very nice simple laptop',
+                            price: 200
+                        }),
+                        new Product({
+                            imagePath: 'https://upload.wikimedia.org/wikipedia/en/7/7b/Personal_Computer.JPG',
+                            title: 'Simple laptop',
+                            description: 'Very nice simple laptop',
+                            price: 200
+                        }),
+                        new Product({
+                            imagePath: 'https://upload.wikimedia.org/wikipedia/en/7/7b/Personal_Computer.JPG',
+                            title: 'Simple laptop',
+                            description: 'Very nice simple laptop',
+                            price: 200
+                        }),
+                        new Product({
+                            imagePath: 'https://upload.wikimedia.org/wikipedia/en/7/7b/Personal_Computer.JPG',
+                            title: 'Simple laptop',
+                            description: 'Very nice simple laptop',
+                            price: 200
+                        }),
+                        new Product({
+                            imagePath: 'https://upload.wikimedia.org/wikipedia/en/7/7b/Personal_Computer.JPG',
+                            title: 'Simple laptop',
+                            description: 'Very nice simple laptop',
+                            price: 200
+                        })
+                        
+                    ]
+                    
+                        for(let i=0; i < products.length; i++){
+                            
+                            Product.find().then((productss)=> {
+                                let count= 0;
+                                for(let j=0; j< productss.length; j++){
+                                    if(products[i].title===productss[j].title){
+                                    count++;
+                                    }
+                                }
+                                if(count==0){
+                                    products[i].save();
+                                }
+                            })
+                            
+                        }
+                        
+                        
+ module.exports = router;
