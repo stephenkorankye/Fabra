4790b1eda439c177748a591d6842dd299a3b1b08
samuelaidoo45 <samuelaidoo45@gmail.com>
2020-04-24 01:44:29 -0400

    Cart

--- routes/index.js
+++ routes/index.js
@@ -49,3 +49,85 @@ router.get('/signup',isLoggedout,(req,res,next)=>{
 router.get('/login', isLoggedin,(req,res,next)=>{
     res.render('routes_UI/login');
 })
+
+
+//My profile endpoint
+router.get('/profile', isLoggedin,(req,res,next)=>{
+    Order.find({userEmail:req.user.email}).then((orders)=>{
+        console.log(orders);
+        res.render('routes_UI/profile', {user:req.user,orders:orders});
+    })
+})
+
+//Reducing item quantity in cart
+router.get('/reduce/:id',(req,res)=> {
+    
+    let cart= new Cart(req.session.cart ? req.session.cart : {} );
+    cart.reduceByOne(req.params.id);
+    cart.generateArray();
+    req.session.cart= cart;
+    
+    if(req.isAuthenticated()){ 
+        User.findOne({email:req.user.email}).then((user)=> {
+            user.cart= cart;  
+            user.save();
+            console.log(user.cart);
+        })
+    }
+    console.log(req.session.cart);   
+    res.redirect('/cart');    
+})
+
+//Removing item from the cart
+router.get('/removeItem/:id',(req,res)=> {
+    
+    let cart= new Cart(req.session.cart ? req.session.cart : {} );
+    cart.removeItem(req.params.id);
+    cart.generateArray();
+    req.session.cart= cart;
+    
+    if(req.isAuthenticated()){ 
+        User.findOne({email:req.user.email}).then((user)=> {
+            user.cart= cart;  
+            user.save();
+            console.log(user.cart);
+        })
+    } 
+    console.log(req.session.cart);
+    res.redirect('/cart');    
+})
+
+//Adding item to cart
+router.put('/add-to-cart/:id',(req,res,next)=> {
+
+    let cart= new Cart(req.session.cart ? req.session.cart : {} );
+    
+    Product.findById(req.params.id).then((product)=> {
+    
+        cart.add(product, product.id);
+        cart.generateArray();
+        req.session.cart= cart;
+        
+        if(req.isAuthenticated()){
+           User.findOne({email:req.user.email}).then((user)=> {
+           user.cart=req.session.cart;
+             user.save().then(()=>{
+                  res.redirect('/'); 
+             })
+        })
+        }else{
+            res.redirect('/'); 
+        }   
+    }); 
+});
+
+
+//Cart Endpoint
+router.get('/cart',(req,res)=> {
+    
+    let cart= req.session.cart || {};
+    let itemsArray= cart.itemsArray ||[];
+    
+    res.render('routes_UI/cart',{cart, itemsArray, user:req.user});
+})
+
