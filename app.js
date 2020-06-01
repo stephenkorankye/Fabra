f8ded172bc8d0da65d1408d3080b4d35254a5a3a
Stephen <korankyes027@gmail.com>
2020-06-01 12:57:35 -0700

    Another Commit

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ require('./config/passport')(passport);
 
 // DB Connection
 const db = require('./config/keys').mongoURI;
-//const db = "mongodb://localhost:27017/fabra" 
+
 
 mongoose.connect(db,{ useNewUrlParser: true , useUnifiedTopology: true })
   .then(() => console.log('Database Connected'))
